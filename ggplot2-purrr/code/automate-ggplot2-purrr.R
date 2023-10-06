03f45bbb871e2fbba09c54c169559f4ffdfd203c
guzjo <josue.guzl98@gmail.com>
2023-10-06 11:01:33 -0600

    Finished R script for plot automation with purrr

--- ggplot2-purrr/code/automate-ggplot2-purrr.R
+++ ggplot2-purrr/code/automate-ggplot2-purrr.R
@@ -23,12 +23,15 @@
 
 library("pacman")     ## for load multiple packages in one function
 
-p_load("ggplot2",     ## for plotting
+p_load("palmerpenguins",
+       "ggplot2",     ## for plotting
+       "ggforce",
        "purrr",       ## for iterative tasks
        "dplyr",       ## for data wrangling
        "glue",        ## to combine variable names into text
        "ggsci",       ## nice themes for plots
-       "patchwork")   ## for multi-panel plots
+       "patchwork",   ## for multi-panel plots
+       "prismatic")   ## idk
 
 
 
@@ -139,7 +142,7 @@ plot_manufacturer <- function(group) {
          title = group)
   
   ggsave(plot = plot, 
-         filename = glue("{group}.plot.png"), 
+         filename = glue("../tmp-figures/{group}.plot.png"), 
          width = 8, height = 8, dpi = 400, 
          bg = "white")
 }
@@ -209,17 +212,222 @@ marginal.plot_manufacturer <- function(group, save = FALSE) {
   
   ## combine plots
   all.plots <- top.bp + plot_spacer() + main.scatter + right.bp + 
-    plot_annotation(title = group) 
-  # + 
-  #   plot_layout(widths = c(1, .05), heights = c(.1, 1))
+    plot_annotation(title = group) + 
+    plot_layout(widths = c(1, .05), heights = c(.1, 1))
 
   # plot_spacer function:
   # https://patchwork.data-imaginist.com/articles/guides/layout.html
-    
-    
-    
+  
+  ## save multi-panel plot
+  if(isTRUE(save)) {
+    ggsave(all.plots,
+           filename = glue("../tmp-figures/{group}.multiplot.png"),
+           width = 6, height = 6)
+  }
+  
+  return(all.plots)
+
+  }
+
+# Now we can apply the function to manufacturers line by line:
+marginal.plot_manufacturer("Nissan", save = TRUE)
+
+
+# Or we iterate over the vector of manufacturers with the {purrr} package.
+map(groups, ~marginal.plot_manufacturer(.x, save = TRUE))
+
+
+# If you wish to only save the visualizations but not plot them, use the walk() function:
+walk(groups, ~marginal.plot_manufacturer(.x, save = TRUE))
+
+
+# variables as inputs
+# To wrap up, let’s consider the use case of exploring the data set. 
+# We create a general function that works with any data set and two numeric variables. 
+# Based on these three inputs, 
+# the function generates a scatter plot with a linear fitting.
+
+
+# In addition, we allow to define a variable to encode points 
+# by color as well as to control the size and transparency of the points. 
+# If the user passes a column for color encoding, 
+# we either use (i) a categorical palette and linear fittings for each group 
+# for qualitative variables or 
+# (ii) a sequential palette with a single smoothing line for quantitative variables.
+
+
+scatter.lm_plot <- function(data, 
+                            var1, 
+                            var2, 
+                            pointsize = 2, 
+                            transparency = .5, 
+                            color = ""){
+  
+  ## check if inputs are valid
+  if(!exists(substitute(data))) stop("data needs to be a dataframe")
+  if(!is.data.frame(data)) stop("data needs to be a dataframe")
+  if(!is.numeric(pull(data[var1]))) stop("Column var1 needs to be of type numeric, passed as string")
+  if(!is.numeric(pull(data[var2]))) stop("Column var2 needs to be of type numeric, passed as string")
+  if(!is.numeric(pointsize)) stop("pointsize needs to be of type numeric")
+  if(!is.numeric(transparency)) stop("transparency needs to be of type numeric")
+  if(color != "") { if(!color %in% names(data)) stop("Column color needs to be a column of data, passed as string") }
+  
+  
+  g <- 
+    ggplot(data, aes(x = !!sym(var1),
+                     y = !!sym(var2))) +
+    geom_point(aes(color = !!sym(color)),
+               size = pointsize,
+               alpha = transparency) +
+    geom_smooth(aes(color = !!sym(color),
+                    color = after_scale(prismatic::clr_darken(color, .3))),
+                method = "lm", se = FALSE) +
+    theme_minimal(base_size = 18) +
+    theme(panel.grid.minor = element_blank(),
+          legend.position = "top")
+  
+  if(color != ""){
+    if(is.numeric(pull(data[color]))){
+      g <- g + scale_color_viridis_c(direction = -1, end = 0.85) +
+        guides(color = guide_colorbar(
+          barwidth = unit(12, "lines"),
+          barheight = unit(0.6, "lines"),
+          title.position = "top"
+        ))
+    } else {
+      g <- g + scale_color_brewer(palette = "Set2")
+    }
+  }
+  
+  return(g)
     
 }
+
+scatter.lm_plot(data = mpg,
+                var1 = "displ",
+                var2 = "hwy")
+
+
+scatter.lm_plot_embraced <- function(data, 
+                            var1, 
+                            var2, 
+                            color = NULL){
+  
+  v1 <- deparse(substitute(var1))
+  v2 <- deparse(substitute(var2))
+  v3 <- deparse(substitute(color))
+  
+  ## check if inputs are valid
+  if(!exists(substitute(data))) stop("data needs to be a dataframe")
+  if(!is.data.frame(data)) stop("data needs to be a dataframe")
+  if(!v1 %in% names(data)) stop("Column var1 needs to be a column of data")
+  if(!v2 %in% names(data)) stop("Column var2 needs to be a column of data")
+  if(!is.numeric(pull(data[v1]))) stop("Column var1 needs to be of type numeric, passed as string")
+  if(!is.numeric(pull(data[v2]))) stop("Column var2 needs to be of type numeric, passed as string")
+  if(!v3 %in% c(names(data), "NULL")) stop("Column color needs to be a column of data")
+
+  
+  g <- 
+    ggplot(data, aes(x = {{var1}},
+                     y = {{var2}})) +
+    geom_point(aes(color = {{color}}),
+               alpha = 0.5) +
+    geom_smooth(aes(color = {{color}}),
+                method = "lm", se = FALSE) +
+    theme_minimal(base_size = 18) +
+    theme(panel.grid.minor = element_blank(),
+          legend.position = "top")
+  
+  if(v3 != "NULL"){
+    if(is.numeric(pull(data[v3]))){
+      g <- g + scale_color_viridis_c(direction = -1, end = 0.85) +
+        guides(color = guide_colorbar(
+          barwidth = unit(12, "lines"),
+          barheight = unit(0.6, "lines"),
+          title.position = "top"
+        ))
+    } else {
+      g <- g + scale_color_brewer(palette = "Set2")
+    }
+  }
+  
+  return(g)
+  
+}
+
+scatter.lm_plot_embraced(data = mpg, var1 = displ, var2 = hwy, color = class)
+
+
+# To iterate over the function, we have two options:
+
+# we can fix one variable and pass the other as vector with map()
+# we can pass two variables as vectors to var1 and var2 with map2()
+# Let’s consider an example in which we vary both positional variables, 
+# using the 2008 car fuel data:
+
+map2(
+  c("displ", "displ", "hwy"), 
+  c("hwy", "cty", "cty"),
+  ~scatter.lm_plot(
+    data = mpg, 
+    var1 = .x, 
+    var2 = .y, 
+    color = "cyl", 
+    pointsize = 3.5
+  )
+)
+
+
+# Now we are going to apply our function to a different data set, 
+# the Palmer penguins to visualize x-y relationships per species. 
+# We automatically iterate over all combinations of a set of chosen numeric variables: 
+# we first generate a vector containing the column names of interest (names) 
+# and then create a data frame with all possible combinations (names_set) 
+# with the help of expand_grid() from the {tidyr} package.
+
+## set up variables of interest
+names <- c("bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g")
+## ... and create all possible combinations
+names_set <- tidyr::expand_grid(names, names)
+
+
+
+
+# Using another mapping function from the {purrr} package called pmap(), 
+# we can map over multiple arguments simultaneously:
+pmap(
+  names_set, 
+  ~scatter.lm_plot(
+    data = penguins, 
+    var1 = .x, var2 = .y, color = "species"
+  )
+)
+
+
+# the facet_matrix() functionality from the {ggforce} package. 
+# Instead of passing variable names in the aesthetics, 
+# we specify placeholders called .panel_x and .panel_y. 
+# We then create our ggplot as usual by adding layers and scales.
+
+# Finally, we specify the variables to use in the facet 
+# inside the facet_matrix() component. 
+# You can also specify specific layers for different areas (upper, diagonal, lower) 
+# inside facet_matrix():
+
+matrix_palmer <- ggplot(penguins, aes(x = .panel_x, 
+                     y = .panel_y)) +
+  geom_point(aes(color = species), alpha = 0.5) +
+  geom_smooth(aes(color = species), method = "lm") +
+  ggforce::geom_autodensity(aes(color = species, fill = after_scale(color)), alpha = 0.7) +
+  scale_color_brewer(palette = "Set2", name = NULL) +
+  ggforce::facet_matrix(vars(names), layer.lower = 2, layer.diag = 3)
+
+
+ggsave(matrix_palmer,
+       filename = glue("../tmp-figures/matrix_palmer.png"),
+       width = 15, height = 10, dpi = 300, bg = "white")
+
+
 
 
 
